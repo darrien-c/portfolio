4203e761640c6ac69dd00fa703756e7177f8fcc9
darrien <46081342+darrienchow@users.noreply.github.com>
2020-06-16 16:46:45 -0700

    fixed fail Seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../css/styles.scss";
 import Layout from '../components/layout';
+import SEO from "../components/seo";
 
 /* Works Section */
 import Works from '../components/works';
